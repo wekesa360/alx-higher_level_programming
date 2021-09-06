154df70303f3265870a61e33b1c3de14e6014743
Michael Wekesa <wekesa360@outlook.com>
2021-09-06 18:52:25 +0300

    Fix: fixed bugs

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 str = "Holberton school"
-print(3*"{}".format(str))
-print(str[:9])
+print(3*"{}".format(str),"\n{}".format(str[:9]))
--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 import this
-print(this, "\n")
+print(this,"\n")
