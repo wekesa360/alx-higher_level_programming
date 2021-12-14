ee99bc886191d3d30af88fb4de79139503f5c876
Michael Wekesa <wekesa360@outlook.com>
2021-12-14 09:37:24 +0300

    Fix: 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,10 @@
 'use strict';
 let nextMax = 0;
 let args = process.argv.slice(2);
-if (args.length > 1){
-    arg.sort();
+if (args.length < 1 || args.length === 1){
+    console.log(0);
+} else{
+    args.sort();
     nextMax = args[args.length - 2];
 }
 console.log(nextMax);
